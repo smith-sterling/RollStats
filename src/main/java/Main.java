675700b335ae44dbea2822839e6e72c2f22c86ef
smith-sterling <104526450+Viper-Fist@users.noreply.github.com>
2023-03-04 10:51:27 -0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, Integer> advRolls = new TreeMap<>();
+        Map<Integer, Integer> disRolls = new TreeMap<>();
+        for (int i = 0; i < 20; ++i) {
+            for (int j = 0; j < 20; ++j) {
+                advRolls.merge(max(i, j) + 1, 1, Integer::sum);
+                disRolls.merge(min(i, j) + 1, 1, Integer::sum);
+            }
+        }
+//        advRolls.forEach((key, value) -> System.out.println(key + ": " + value));
+//        disRolls.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        List<Integer> advRollsArray = new ArrayList<>();
+        advRolls.forEach((key, value) -> { for (int i = 0; i < value; ++ i) { advRollsArray.add(key); } });
+        printMean(advRollsArray);
+        printMedian(advRollsArray);
+
+        List<Integer> disRollsArray = new ArrayList<>();
+        disRolls.forEach((key, value) -> { for (int i = 0; i < value; ++ i) { disRollsArray.add(key); } });
+        printMean(disRollsArray);
+        printMedian(disRollsArray);
+    }
+    public static void printMean(List<Integer> values) {
+        System.out.println("The mean is " +
+                (double)values.stream().mapToInt(value -> value).sum() / values.size());
+        //or, alternatively,
+//        System.out.println("The mean is " +
+//                values.stream().collect(Collectors.averagingDouble(Integer::doubleValue)));
+    }
+    public static void printMedian(List<Integer> values) {
+        System.out.println("The median is " +
+                values.stream()
+                        .sorted()
+                        .collect(Collectors.collectingAndThen(
+                                Collectors.toList(), list -> { return list.size() % 2 == 0 ?
+                                            (list.get(list.size() / 2 - 1) + list.get(list.size() / 2)) / 2.0 :
+                                            list.get(list.size() / 2);
+                                })));
+//        values.sort(Integer::compareTo);
+//        System.out.println("The median is " + values.get(values.size() / 2));
+    }
+}
